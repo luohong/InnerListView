6bb0650952ac4c7dd6826cd42da252a15ff2a742
luohong722 <luohong722@yahoo.cn>
2014-08-26 18:25:56 +0800

    导入项目

--- src/com/sun/shine/study/innerscrollview/view/InnerListView.java
+++ src/com/sun/shine/study/innerscrollview/view/InnerListView.java
@@ -9,71 +9,60 @@ import android.widget.ScrollView;
 
 public class InnerListView extends ListView {
 
-    ScrollView parentScrollView;
+	private int maxHeight;
 
-    public ScrollView getParentScrollView() {
-        return parentScrollView;
-    }
+	private ScrollView parentScrollView;
 
-    public void setParentScrollView(ScrollView parentScrollView) {
-        this.parentScrollView = parentScrollView;
-    }
+	public ScrollView getParentScrollView() {
+		return parentScrollView;
+	}
 
-    private int maxHeight;
+	public void setParentScrollView(ScrollView parentScrollView) {
+		this.parentScrollView = parentScrollView;
+	}
 
-    public int getMaxHeight() {
-        return maxHeight;
-    }
+	public int getMaxHeight() {
+		return maxHeight;
+	}
 
-    public void setMaxHeight(int maxHeight) {
-        this.maxHeight = maxHeight;
-    }
+	public void setMaxHeight(int maxHeight) {
+		this.maxHeight = maxHeight;
+	}
 
-    public InnerListView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-    }
+	public InnerListView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        // TODO Auto-generated method stub
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		if (maxHeight > -1) {
+			heightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST);
+		}
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
 
-        if (maxHeight > -1) {
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		switch (ev.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			setParentScrollAble(false);
+		case MotionEvent.ACTION_MOVE:
+			break;
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_CANCEL:
+			setParentScrollAble(true);
+			break;
+		default:
+			break;
+		}
+		return super.onInterceptTouchEvent(ev);
+	}
 
-            heightMeasureSpec = MeasureSpec.makeMeasureSpec(maxHeight, MeasureSpec.AT_MOST);
-
-        }
-
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        System.out.println(getChildAt(0));
-    }
-
-    @Override
-    public boolean onInterceptTouchEvent(MotionEvent ev) {
-
-        switch (ev.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-
-                setParentScrollAble(false);
-            case MotionEvent.ACTION_MOVE:
-
-                break;
-            case MotionEvent.ACTION_UP:
-
-            case MotionEvent.ACTION_CANCEL:
-                setParentScrollAble(true);
-                break;
-            default:
-                break;
-        }
-        return super.onInterceptTouchEvent(ev);
-    }
-
-    /**
-     * @param flag
-     */
-    private void setParentScrollAble(boolean flag) {
-
-        parentScrollView.requestDisallowInterceptTouchEvent(!flag);
-    }
+	/**
+	 * @param flag
+	 */
+	private void setParentScrollAble(boolean flag) {
+		parentScrollView.requestDisallowInterceptTouchEvent(!flag);
+	}
 
 }
